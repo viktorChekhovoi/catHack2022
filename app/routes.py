0f8b4d285388e2ac2813ab5277d3f1931796938d
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2022-06-23 08:31:29 -0600

    Working on email verification

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
-from flask import render_template
+from flask import render_template, request
+from flask_mail import *
 from app import app
+from random import *
 
-
+EMAIL_PATTERN = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['POST'])
 def index():
-    return render_template('index.html')
+    otp = randint(0,999999)   
+    if 'otp' in request.form:
+        user_otp = request.form['otp']
+        if user_otp == otp:
+            return render_template('dashboard.html')
+        else:
+            return render_template('index.html', pattern=EMAIL_PATTERN, status = 'wrong otp')
+    if 'email' in request.form:
+        return render_template('index.html', pattern=EMAIL_PATTERN, status = 'email received')
+    else:
+        return render_template('index.html', pattern=EMAIL_PATTERN, status = 'waiting for email')
+
+
