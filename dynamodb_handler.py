5bd7ff69db7183b10a372622a1e439423359e8a7
nick.chun <nick.chun@cat.com>
2022-06-23 09:45:56 -0700

    Added DB functionality

--- dynamodb_handler.py
+++ dynamodb_handler.py
@@ -1,160 +1,105 @@
-# import boto3
 from boto3 import client, resource
-from decouple import config
 
-AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-REGION_NAME           = config("REGION_NAME")
-# AWS_SESSION_TOKEN     = config("AWS_SESSION_TOKEN")
+AWS_ACCESS_KEY_ID     = "AKIAZQPIPR67ETFIMMEG"
+AWS_SECRET_ACCESS_KEY = "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"
+REGION_NAME           = "us-east-1"
 
 client = client(
     'dynamodb',
     aws_access_key_id     = AWS_ACCESS_KEY_ID,
     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
-    # aws_session_token     = AWS_SESSION_TOKEN,
+    region_name           = REGION_NAME
 )
 
 resource = resource(
     'dynamodb',
     aws_access_key_id     = AWS_ACCESS_KEY_ID,
     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
-    # aws_session_token     = AWS_SESSION_TOKEN,
+    region_name           = REGION_NAME
 )
-# BookTable = DynamoDB.Table('Book')
 
-'''
-    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Client.create_table
-    Create a new table
-'''
-def CreatATableBook():
-        
-    client.create_table(
-        AttributeDefinitions = [ #array of attributes (name and type)
-            {
-                'AttributeName': 'id', # Name of the attribute
-                'AttributeType': 'N'   # N -> Number (S -> String, B-> Binary)
-            }
-        ],
-        TableName = 'Book', # Name of the table 
-        KeySchema = [       # 
-            {
-                'AttributeName': 'id',
-                'KeyType'      : 'HASH' # HASH -> partition key, RANGE -> sort key
-            }
-        ],
-        BillingMode = 'PAY_PER_REQUEST',
-        Tags = [ # OPTIONAL 
-            {
-                'Key' : 'test-resource',
-                'Value': 'dynamodb-test'
+UserTable = resource.Table('user-table')
 
-            }
-        ]
-    )
+def addUser(email, company, password, role):
 
-
-'''
-    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Client.delete_table
-    Delete a table
-'''
-
-
-'''
-    https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.03.html
-    CRUD Operations
-'''
-BookTable = resource.Table('Book')
-
-def addItemToBook(id, title, author):
-
-    response = BookTable.put_item(
+    response = UserTable.put_item(
         Item = {
-            'id'     : id,
-            'title'  : title,
-            'author' : author,
-            'likes'  : 0
+            'email'     : email,
+            'company'  : company,
+            'password' : password,
+            'role'  : role
         }
     )
 
     return response
 
-def GetItemFromBook(id):
+def GetUser(email):
 
-    response = BookTable.get_item(
+    response = UserTable.get_item(
         Key = {
-            'id'     : id
+            'email'     : email
         },
         AttributesToGet=[
-            'title', 'author' # valid types dont throw error, 
+            'email', 'company', 'role' # valemail types dont throw error, 
                               # Other types should be converted to python type before sending as json response
         ]
     )
 
     return response
 
-def UpdateItemInBook(id, data:dict):
+def UpdateUserPassword(email, password):
 
-    response = BookTable.update_item(
+    response = UserTable.update_item(
         Key = {
-            'id': id
+            'email': email
         },
         AttributeUpdates={
-            'title': {
-                'Value'  : data['title'],
-                'Action' : 'PUT' # # available options -> DELETE(delete), PUT(set), ADD(increment)
-            },
-            'author': {
-                'Value'  : data['author'],
-                'Action' : 'PUT'
+            'password': {
+                'Value'  : password,
+                'Action' : 'PUT' # available options -> DELETE(delete), PUT(set), ADD(increment)
             }
         },
         ReturnValues = "UPDATED_NEW"  # returns the new updated values
     )
     return response
 
-def LikeABook(id):
+def UpdateUserRole(email, role):
 
-    response = BookTable.update_item(
+    response = UserTable.update_item(
         Key = {
-            'id': id
+            'email': email
         },
-        AttributeUpdates = {
-            'likes': {
-                'Value'  : 1,
-                'Action' : 'ADD'
+        AttributeUpdates={
+            'role': {
+                'Value'  : role,
+                'Action' : 'PUT'
             }
         },
         ReturnValues = "UPDATED_NEW"
     )
-    response['Attributes']['likes'] = int(response['Attributes']['likes'])
-
     return response
 
-def ModifyAuthorforBook(id, author):
+# def ModifyAuthorforBook(email, author):
 
-    response = BookTable.update_item(
+#     response = UserTable.update_item(
+#         Key = {
+#             'email': email
+#         },
+#         UpdateExpression = 'SET info.author = :new_author', #set author to new value
+#         #ConditionExpression = '', # execute until this condition fails # no condition
+#         ExpressionAttributeValues = { # Value for the variables used in the above expressions
+#             ':new_author': author
+#         },
+#         ReturnValues = "UPDATED_NEW"  #what to return
+#     )
+#     return response
+
+def DeleteUser(email):
+
+    response = UserTable.delete_item(
         Key = {
-            'id': id
-        },
-        UpdateExpression = 'SET info.author = :new_author', #set author to new value
-        #ConditionExpression = '', # execute until this condition fails # no condition
-        ExpressionAttributeValues = { # Value for the variables used in the above expressions
-            ':new_author': author
-        },
-        ReturnValues = "UPDATED_NEW"  #what to return
-    )
-    return response
-
-def DeleteAnItemFromBook(id):
-
-    response = BookTable.delete_item(
-        Key = {
-            'id': id
+            'email': email
         }
     )
 
     return response
-    
-# batch get items resource
