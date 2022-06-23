45ab2022226fa82432716e276e5d38f6e849590e
prayag99 <prayagpatel1999@gmail.com>
2022-06-23 14:56:49 -0500

    Updating timestamp in the datalog  after user successfully logs in.

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,17 @@ from flask_mail import *
 from app import app
 from random import *
 from validate_email_address import validate_email
+import datetime
+import dynamodb_handler
 
-
-app.config["MAIL_SERVER"]='smtp.gmail.com'  
-app.config["MAIL_PORT"] = 465      
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = 'cathack.notification@gmail.com'
-app.config['MAIL_PASSWORD'] = 'wjbdsvjkndsntrkw' 
-app.config['MAIL_USE_TLS'] = False  
-app.config['MAIL_USE_SSL'] = True  
+app.config['MAIL_PASSWORD'] = 'wjbdsvjkndsntrkw'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
-mail = Mail(app)  
+mail = Mail(app)
 otp = randint(100000,999999)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,19 +22,22 @@ def index():
     if 'otp' in request.form:
         userOtp = request.form['otp']
         if int(userOtp) == otp:
+            now = datetime.datetime.now()
+            date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+            dynamodb_handler.updatelog(mail, date_time)
             return jsonify({
                 'status': 'authenticated'
             })
-        else: 
+        else:
             return jsonify({
                 'status': 'wrong_otp'
             })
-    if 'email' in request.form:  
+    if 'email' in request.form:
         userEmail = request.form['email']
         if validate_email(userEmail, verify=True):
-            msg = Message('OTP',sender = 'username@gmail.com', recipients = [request.form['email']])  
-            msg.body = str(otp)  
-            mail.send(msg)  
+            msg = Message('OTP',sender = 'username@gmail.com', recipients = [request.form['email']])
+            msg.body = str(otp)
+            mail.send(msg)
             return jsonify({
                 'status': 'requested_otp'
             })
@@ -49,5 +53,3 @@ def index():
 @app.route('/dash', methods=['POST'])
 def dashboard():
     return jsonify({})
-
-
