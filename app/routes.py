99d65e984a2cf965f9694c42b1bc3ae525cacf60
Aaryanmukherjee <65692566+Aaryanmukherjee@users.noreply.github.com>
2022-06-23 15:30:29 -0700

    cors

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,36 @@
-from flask import redirect, render_template, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify
 from flask_mail import *
 from app import app
 from random import *
 from validate_email_address import validate_email
 import datetime
 import dynamodb_handler
+from flask_cors import CORS, cross_origin
+from flask_session import Session
 
+
+app = Flask(__name__)
 app.config["MAIL_SERVER"]='smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = 'cathack.notification@gmail.com'
 app.config['MAIL_PASSWORD'] = 'wjbdsvjkndsntrkw'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 mail = Mail(app)
 otp = randint(100000,999999)
+server_session = Session(app)
 
-@app.route('/', methods=['GET', 'POST'])
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/api/v1/users")
+def list_users():
+  return "user example"
+
 @app.route('/index', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def index():
     if 'otp' in request.form:
         userOtp = request.form['otp']
